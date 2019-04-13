58ad25e5b8970266211bfaacc4e3810f5de68412
Ben Piotti <benpiotti@Bens-MacBook-Pro.local>
2019-04-12 20:24:29 -0400

    first commit: Added basic toolbar and hoc components

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Toolbar.css';
+
+const toolbar = () => (
+    <header className="Toolbar">
+
+    </header>
+);
+
+export default toolbar;
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Aux from '../Aux/Aux'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar.js'
+import './Layout.css'
+
+class Layout extends React.Component {
+    render () {
+        return (
+            <Aux>
+                <Toolbar/>
+                <main className="Content">
+                    {this.props.children}
+                </main>
+            </Aux>
+            )
+    }
+}
+
+export default Layout
